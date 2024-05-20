4115f9c82df542459589c2d5dd570339a36b60ce
vpele <vpele@WOLODYMYR>
2024-05-20 07:54:33 +0200

    Add project files.

---
+++ KursWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KursWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
