e52d2b774a86043564c6437383e717313f322111
0WhyMe0 <wolodymyrpelekh@gmail.com>
2024-05-28 22:09:36 +0200

    Finish Configuration batton EDIT

--- KursWeb/Controllers/CategoryController.cs
+++ KursWeb/Controllers/CategoryController.cs
@@ -30,7 +30,7 @@ namespace KursWeb.Controllers
             {
                 ModelState.AddModelError("name", "The Display Order cannot exactly match the Name.");
             }
-            //suka
+            
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
@@ -38,6 +38,66 @@ namespace KursWeb.Controllers
                 return RedirectToAction("Index");
             }
             return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(obj);
+
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
